78c277109b0d9e0eeaeeb53b27f050bf01caab0c
Michael Kreil <github@michael-kreil.de>
2016-10-20 01:18:34 +0200

    qr code support

--- bin/server.js
+++ bin/server.js
@@ -8,9 +8,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const htmlencode = require('htmlencode');
 const child_process = require('child_process');
+const qrcode = require('qrcode-generator');
 
 const template = fs.readFileSync(path.resolve(__dirname, '../web/index.html'), 'utf8');
- 
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/assets', express.static(path.resolve(__dirname, '../web/assets')));
@@ -32,30 +33,88 @@ function getPage() {
 }
 
 function print(text) {
-	let size = [1600, 720];
+	let size, args, svg;
 
-	let args = [
-		'-colorspace','Gray',
-		'-depth','8',
-		'-size','6000x300',
-		'canvas:black',
-		'-fill','white',
-		'-stroke','white',
-		'-pointsize','200',
-		'-draw','text 0,200 "'+text.replace(/\"/g, c => '\\"')+'"',
-		'-trim',
-		'-resize',size.join('x')+'>',
-		'-gravity','center',
-		'-background','black',
-		'-extent',size.join('x'),
-		'-rotate','90',
-		'-flop',
-		'-depth','1',
-		'gray:-'
-	];
+	if (text.startsWith('http')) {
+
+		let ok = false, version = 1, qr;
+
+		while (!ok) {
+			try {
+				qr = qrcode(version, 'M');
+				qr.addData(text);
+				qr.make();
+				ok = true;
+			} catch (e) {
+				version++;
+			}
+		}
+
+		svg = qr.createSvgTag(1, 0);
+		var qrsize = svg.match(/width\=\"([0-9]+)/i)[1];
+		var density = Math.floor(72*720/qrsize);
+		console.log(density);
+
+		let smallSize = [720, 150];
+		size = [smallSize[0], smallSize[0]+smallSize[1]];
+		args = [
+			'-colorspace','Gray',
+			'-depth','8',
+			'-size',(text.length*150)+'x200',
+			'canvas:white',
+			'-fill','black',
+			'-stroke','black',
+			'-pointsize','150',
+			'-draw','text 0,150 "'+text.replace(/\"/g, c => '\\"')+'"',
+			'-trim',
+			'-resize',smallSize.map(v => v-20).join('x')+'>',
+			'-gravity','center',
+			'-background','white',
+			'-extent',smallSize.join('x'),
+			'-gravity','south',
+			'-extent',size.join('x'),
+			'-gravity','North',
+			'(',
+				'-density', density,
+				'-size',[qrsize,qrsize].join('x'),
+				'svg:-',
+			')',
+			'-composite',
+			'-negate',
+			'-flop',
+			'-colorspace','Gray',
+			'-depth','1',
+			'gray:-'
+		];
+	} else {
+		size = [1600, 720];
+		args = [
+			'-colorspace','Gray',
+			'-depth','8',
+			'-size',(text.length*300)+'x400',
+			'canvas:black',
+			'-fill','white',
+			'-stroke','white',
+			'-pointsize','300',
+			'-draw','text 0,300 "'+text.replace(/\"/g, c => '\\"')+'"',
+			'-trim',
+			'-resize',size.map(v => v-200).join('x')+'>',
+			'-gravity','center',
+			'-background','black',
+			'-extent',size.join('x'),
+			'-rotate','90',
+			'-flop',
+			'-depth','1',
+			'gray:-'
+		];
+		size.reverse();
+	}
 
 	var result = [];
 	var child = child_process.spawn('convert', args);
+	
+	if (svg) child.stdin.end(svg);
+
 	child.stderr.on('data', data => console.error(data.toString()));
 	child.stdout.on('data', data => result.push(data));
 	child.on('close', () => {
@@ -65,13 +124,13 @@ function print(text) {
 		let pos = 0;
 
 		write([0x1b,'@']); // Init
-		write([0x1b,'i','z',0xa6,0x0a,0x3e,0,size[0] & 0xff,size[0] >> 8,0,0,0,0]); // Set media type
+		write([0x1b,'i','z',0xa6,0x0a,0x3e,0,size[1] & 0xff,size[1] >> 8,0,0,0,0]); // Set media type
 		write([0x1b,'i','K']); // Set cut type
 		write([0x1b,'i','A']); // Enable cutter
 		write([0x1b,'i','d',0,0]); // Set margin = 0
 
-		var lb = Math.floor(size[1] / 8) + (size[1] % 8 > 0 ? 1 : 0);
-		for (let y = 0; y < size[0]; y++) {
+		var lb = Math.floor(size[0] / 8) + (size[0] % 8 > 0 ? 1 : 0);
+		for (let y = 0; y < size[1]; y++) {
 			write(['g', 0x00, 90]);
 			copy(result.slice(y*lb, (y+1)*lb));
 			for (let x = lb; lb < 90; lb++) write([0x00]);
@@ -80,7 +139,7 @@ function print(text) {
 		write([0x1a]); // Print
 
 		if (debug) {
-			var child = child_process.spawn('convert', ['-size',size.reverse().join('x'),'-depth',1,'gray:-','temp.png']);
+			var child = child_process.spawn('convert', ['-size',size.join('x'),'-depth',1,'gray:-','temp.png']);
 			child.stdin.end(result);
 		} else {
 			fs.writeFile('/dev/usb/lp0', buffer.slice(0,pos));
@@ -98,7 +157,7 @@ function print(text) {
 			data.copy(buffer, pos);
 			pos += data.length;
 		}
-		fs.writeFileSync('temp.bin', result);
+		//fs.writeFileSync('temp.bin', result);
 	});
 }
 
